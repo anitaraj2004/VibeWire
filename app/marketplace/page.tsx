913731ed5974fe29ef3ad881f51bc8004db83fca
Sandra-Cai <109099733+Sandra-Cai@users.noreply.github.com>
2023-11-11 17:29:19 -0500

    Update page.tsx

--- app/marketplace/page.tsx
+++ app/marketplace/page.tsx
@@ -1,1 +1,12 @@
+const sdk = require('api')('@verbwire/v1.0#cfbl71zelo76w7am');
 
+sdk.auth('sk_live_e0fe1549-697a-489c-9b3c-662f93ad9489');
+sdk.postNftMintQuickmintfromfile({
+  allowPlatformToOperateToken: 'true',
+  chain: 'bsc-testnet',
+  name: 'VibeWire',
+  description: 'VibeWire',
+  recipientAddress: 'vibewiretoken'
+})
+  .then(({ data }) => console.log(data))
+  .catch(err => console.error(err));
