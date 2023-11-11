0a357669907286b49a18d820eb59fa8bc42d7026
Sandra-Cai <109099733+Sandra-Cai@users.noreply.github.com>
2023-11-11 17:26:19 -0500

    Update page.tsx

--- app/marketplace/page.tsx
+++ app/marketplace/page.tsx
@@ -1,10 +1,1 @@
-curl --request POST \
-     --url https://api.verbwire.com/v1/nft/mint/quickMintFromFile \
-     --header 'X-API-Key: sk_live_e0fe1549-697a-489c-9b3c-662f93ad9489' \
-     --header 'accept: application/json' \
-     --header 'content-type: multipart/form-data' \
-     --form allowPlatformToOperateToken=true \
-     --form chain=bsc-testnet \
-     --form name=VibeWire \
-     --form description=VibeWire \
-     --form recipientAddress=vibewiretoken
+
